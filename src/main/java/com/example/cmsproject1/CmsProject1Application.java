243a760c4cd4fd1bde5968b26b11a1a4d6b9f54c
Minhyo <minhyo17@gmail.com>
2023-12-11 18:40:28 +0900

    Initial commit

---
+++ src/main/java/com/example/cmsproject1/CmsProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.cmsproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cmsproject1/CmsProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cmsproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
